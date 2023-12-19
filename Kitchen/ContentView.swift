b4bfd26d52e98acfdd90090a1fb1125968a623a7
berdogan20 <berdogan20@ku.edu.tr>
2023-12-19 14:26:45 +0300

    First commit to try

--- Kitchen/ContentView.swift
+++ Kitchen/ContentView.swift
@@ -10,9 +10,9 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
+            Text("Trial")
+            Text("Trial 2")
+            Text("Trial 3")
             Text("Hello, world!")
         }
         .padding()

6a0b5e1772ad0a57f69f504904474c7db913087d
berdogan20 <berdogan20@ku.edu.tr>
2023-12-19 14:08:04 +0300

    Initial Commit

---
+++ Kitchen/KitchenApp.swift
@@ -0,0 +1,17 @@
+//
+//  KitchenApp.swift
+//  Kitchen
+//
+//  Created by beyza erdoğan on 19.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct KitchenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

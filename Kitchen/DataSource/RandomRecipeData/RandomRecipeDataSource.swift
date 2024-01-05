c2a2a3e93913744f4ed0a3b787f41271fca38175
berdogan20 <berdogan20@ku.edu.tr>
2024-01-05 13:58:18 +0300

    Random Recipe API call is done but needs design

---
+++ Kitchen/DataSource/RandomRecipeData/RandomRecipeDataSource.swift
@@ -0,0 +1,59 @@
+//
+//  RandomRecipeDataSource.swift
+//  Kitchen
+//
+//  Created by beyza erdoğan on 5.01.2024.
+//
+
+import Foundation
+
+struct RandomRecipeDataSource {
+
+    private let baseURL = "https://api.spoonacular.com/recipes/random?apiKey=1ff1bdf6a5844a5bb5270a7fabc74fa8&includeNutrition=true."
+    var delegate: RandomRecipeDataSourceDelegate?
+
+    func loadRandomRecipeData(){
+
+        // get shared URL session
+        let session = URLSession.shared
+
+        // create a URL
+        // returns an optional String
+        guard let url = URL(string: baseURL) else {return}
+
+        // create a URL request
+        var request = URLRequest(url: url)
+
+        // set the HTTP verb
+        request.httpMethod = "GET"
+
+        // set header for JSON
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+
+
+        // create a data task
+        let dataTask = session.dataTask(with: request) { data, response, error in
+            guard let data else {return}    // if the data is nil, return
+
+            // print(String(decoding: data, as: UTF8.self))   // if you receive completion handler, do this
+
+            let decoder = JSONDecoder()
+            do{
+                let randomRecipeData = try decoder.decode(RandomRecipe.self, from: data)
+                // print(randomRecipeData)
+
+                DispatchQueue.main.async {
+                    // put to main thread
+                    delegate?.randomRecipeDataLoaded(data: randomRecipeData)
+                }
+            }
+            catch{
+                print(error)
+            }
+        }
+
+        // resume the data task
+        dataTask.resume()
+    }
+
+}
---
+++ Kitchen/Views/RandomRecipeViews/RandomRecipeViewModel.swift
@@ -0,0 +1,31 @@
+//
+//  RandomRecipeViewModel.swift
+//  Kitchen
+//
+//  Created by beyza erdoğan on 5.01.2024.
+//
+import Foundation
+
+class RandomRecipeViewModel: ObservableObject {
+
+    @Published var isLoading = true
+    @Published var randomRecipe: RandomRecipe = RandomRecipe(recipes: [])
+    private var randomRecipeDataSoruce = RandomRecipeDataSource()
+
+    init(){
+        randomRecipeDataSoruce.delegate = self
+    }
+
+    func loadRandomRecipeData(){
+        randomRecipeDataSoruce.loadRandomRecipeData()
+    }
+
+
+}
+
+extension RandomRecipeViewModel: RandomRecipeDataSourceDelegate {
+    func randomRecipeDataLoaded(data: RandomRecipe) {
+        isLoading = false
+        randomRecipe = data
+    }
+}

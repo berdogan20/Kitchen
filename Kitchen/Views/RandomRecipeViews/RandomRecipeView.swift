15160ff27ff6e20463883348106e87543f01a6ef
Meryem Karakaş <72557756+MeryemKarakas13@users.noreply.github.com>
2024-01-07 15:11:57 +0300

    random recipe design

--- Kitchen/Views/RandomRecipeViews/RandomRecipeView.swift
+++ Kitchen/Views/RandomRecipeViews/RandomRecipeView.swift
@@ -14,82 +14,95 @@ struct RandomRecipeView: View {
 
     var body: some View {
         if viewModel.isLoading {
+            Spacer()
             Loading(text: "Recipe is Coming!!!")
-                .onAppear(){
+                .onAppear() {
                     viewModel.loadRandomRecipeData()
                 }
+            Spacer()
         } else {
             NavigationView {
-                ScrollView(.horizontal, showsIndicators: false) {
-                    let recipe = viewModel.randomRecipe.recipes.first
-
-                    Text(recipe?.title ?? "")
-                        .font(.headline)
-                        .foregroundColor(.black)
-
-                    AsyncImage(url: URL(string: recipe?.image ?? "")) { phase in
-                        if let image = phase.image {
-                            image
-                                .resizable()
-                                .aspectRatio(contentMode: .fill)
-                                .frame(width: 300, height: 200) // Fixed size
-                                .cornerRadius(Radius.radius_5)
-                                .clipped() // Clip the image to prevent overflow
-                        } else if phase.error != nil {
-                            Text("Image not found.")
-                                .frame(width: 100, height: 100)
-                        }
-                    }
-                    .padding(Spacing.spacing_2)
-
+                VStack {
+                    Spacer()
                     HStack {
-                        Image(systemName: "clock")
-                        Text("\(recipe?.readyInMinutes ?? 0) mins")
-                    }
-                    .foregroundColor(.gray)
+                        Spacer()
+                        VStack(alignment: .center) {
 
-                    HStack {
-                        Image(systemName: "person.2")
-                        Text("\(recipe?.servings ?? 0) servings")
-                    }
-                    .foregroundColor(.gray)
+                            let recipe = viewModel.randomRecipe.recipes.first
 
-                    if let recipe = recipe {
-                        VStack {
-                            RecipeInfoView(imageName: recipe.glutenFree ? "glutenFree" : "", text: recipe.glutenFree ? "Gluten Free" : "not Gluten Free")
-                            RecipeInfoView(imageName: recipe.dairyFree ? "dairyFree" : "", text: recipe.dairyFree ? "Dairy Free" : "not Dairy Free")
-                            RecipeInfoView(imageName: recipe.vegetarian ? "vegetarian" : "", text: recipe.vegetarian ? "vegetarian" : "not Begetarian")
-                            RecipeInfoView(imageName: recipe.vegan ? "vegan" : "", text: recipe.vegan ? "Vegan" : "not Vegan")
-                            RecipeInfoView(imageName: recipe.veryHealthy ? "veryHealthy" : "", text: recipe.veryHealthy ? "Healthy" : "not Healthy")
+                            Text(recipe?.title ?? "")
+                                .font(.headline)
+                                .foregroundColor(.black)
+                                .lineLimit(Int.max)
+                                .multilineTextAlignment(.center) // Center the text
+
+                            AsyncImage(url: URL(string: recipe?.image ?? "")) { phase in
+                                if let image = phase.image {
+                                    image
+                                        .resizable()
+                                        .aspectRatio(contentMode: .fill)
+                                        .cornerRadius(Radius.radius_5)
+                                        .clipped()
+                                        .frame(width: 300, height: 200)
+                                } else if phase.error != nil {
+                                    Text("Image not found.")
+                                        .frame(width: 100, height: 100)
+                                }
+                            }
+                            .padding(Spacing.spacing_2)
+
+                            HStack {
+                                Image(systemName: "clock")
+                                Text("\(recipe?.readyInMinutes ?? 0) mins")
+                            }
+                            .foregroundColor(.gray)
+
+                            HStack {
+                                Image(systemName: "person.2")
+                                Text("\(recipe?.servings ?? 0) servings")
+                            }
+                            .foregroundColor(.gray)
+
+                            if let recipe = recipe {
+                                VStack (alignment: .center){
+                                    RecipeInfoView(imageName: recipe.glutenFree ? "glutenFree" : "gluten", text: recipe.glutenFree ? "Gluten Free" : "not Gluten Free")
+                                    RecipeInfoView(imageName: recipe.dairyFree ? "dairyFree" : "dairy", text: recipe.dairyFree ? "Dairy Free" : "not Dairy Free")
+                                    RecipeInfoView(imageName: recipe.vegetarian ? "vegetarian" : "notVegetarian", text: recipe.vegetarian ? "Vegetarian" : "not Vegetarian")
+                                    RecipeInfoView(imageName: recipe.vegan ? "vegan" : "notVegan", text: recipe.vegan ? "Vegan" : "not Vegan")
+                                    RecipeInfoView(imageName: recipe.veryHealthy ? "veryHealthy" : "notHealthy", text: recipe.veryHealthy ? "Healthy" : "not Healthy")
+                                }
+
+                            } else {
+                                Text("No Recipe Available")
+                            }
+
                         }
-                    } else {
-                        Text("No Recipe Available")
-                    }
-                    
 
+                        Spacer()
+                    }
+                    Spacer()
                 }
                 .navigationBarTitle("Random Recipe")
+                .background(Color.white.edgesIgnoringSafeArea(.all))
             }
         }
     }
 }
-
 
 struct RecipeInfoView: View {
     let imageName: String
     let text: String
 
     var body: some View {
-        HStack(spacing: 10) {
+        HStack {
             Image(imageName)
                 .resizable()
                 .frame(width: 20, height: 20)
-                .opacity(imageName.isEmpty ? 0 : 1) // Hide the image if the name is empty
+                .opacity(imageName.isEmpty ? 0 : 1)
             Text(text)
         }
     }
 }
-
 
 struct RandomRecipeView_Previews: PreviewProvider {
     static var previews: some View {

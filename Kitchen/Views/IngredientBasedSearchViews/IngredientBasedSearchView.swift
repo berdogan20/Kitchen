300a14fadc4b5b1fd50e36e5df54ecb7c4ba3438
yomur21 <yomur21@ku.edu.tr>
2024-01-07 20:40:00 +0300

    Added a navigation link to the ingredient search recipes and the recipes by category. They pop up one by one for now.

--- Kitchen/Views/IngredientBasedSearchViews/IngredientBasedSearchView.swift
+++ Kitchen/Views/IngredientBasedSearchViews/IngredientBasedSearchView.swift
@@ -23,6 +23,8 @@ struct IngredientBasedSearchView: View {
                     viewModel.loadIngredientBasedSearchData(searchIngredients: searchIngredients)
                 }
         } else {
+
+            /*
             NavigationView {
                 List {
                     ForEach(0 ..< viewModel.ingredientBasedSearch.count / 2, id: \.self) { index in
@@ -38,6 +40,20 @@ struct IngredientBasedSearchView: View {
                         }
                         .padding(.horizontal, Spacing.spacing_2) // Add horizontal padding
                     }
+                }
+                .navigationTitle("Recipes")
+            }
+
+            */
+            NavigationView {
+                List(viewModel.ingredientBasedSearch, id: \.self) { recipe in
+                    HStack(spacing: Spacing.spacing_2) {
+                        NavigationLink(destination: RecipeDetailView(recipeID: recipe.id)) {
+                            RecipeView(recipe: recipe)
+                                .frame(maxWidth: .infinity) // Fill available space
+                        }
+                    }
+                    .padding(.horizontal, Spacing.spacing_2) // Add horizontal padding
                 }
                 .navigationTitle("Recipes")
             }
